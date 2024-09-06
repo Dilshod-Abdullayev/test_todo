4704e1db7899f24845d4f43d0b3ade052805b7d5
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2024-09-06 14:56:44 +0500

    file configuration

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,8 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  roots: ['<rootDir>/src'], 
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'], // Test fayllarining nomlanishi
 };
 
 export default config;
